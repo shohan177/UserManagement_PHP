eb1f99370f73f7d6a4261765d7dc2abe40e326f5
sarwar jahan shohan <sarwar.shohan24@gmail.com>
2020-05-18 20:24:54 +0600

    upload project file

--- app/db.php
+++ app/db.php
@@ -5,7 +5,7 @@
 		$server = 'localhost';
 		$user = 'root';
 		$pass = '';
-		$db_name = 'shohan';
+		$db_name = 'users';
 		$connection = new mysqli($server,$user,$pass,$db_name);
 
  ?>
--- index.php
+++ index.php
@@ -1,10 +1,39 @@
 <?php require_once "app/db.php" ?>
 <?php require_once "app/function.php" ?>
+<?php 
+	//session start
+	session_start();
+	if (isset($_SESSION['id']) AND isset($_SESSION['email']) AND isset($_SESSION['phone'])) {
+		header("location:profile.php");
+	}
+
+	//relogin with cooki
+	if (isset($_COOKIE['relog'])) {
+		$user_id = $_COOKIE['relog'];
+
+		// data recive by cookii id
+		$sql = "SELECT * FROM user_info WHERE id = '$user_id'";
+				$data = $connection -> query($sql);
+				$login_user = $data -> fetch_assoc();
+
+				// session create by cooki id
+				$_SESSION['id'] = $login_user['id'];
+				$_SESSION['name'] = $login_user['name'];
+				$_SESSION['email'] = $login_user['email'];
+				$_SESSION['phone'] = $login_user['phone'];
+				$_SESSION['photo'] = $login_user['photo'];
+				$_SESSION['uname'] = $login_user['uname'];
+
+				// redirect user to profile page
+				header("location:profile.php");
+	}
+ ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title>login</title>
 
 	<link rel="stylesheet" href="asset/css/responsive.css">
 	<link rel="stylesheet" href="asset/css/bootstrap.min.css">
@@ -17,88 +46,142 @@
 		
 		
 		if(isset($_POST['save'])){
-		$name = $_POST['name'];
-		$email = $_POST['email'];
-		$phone = $_POST['phone'];
-		$Address = $_POST['add'];
+		$user_name = $_POST['name'];
+		$user_pass = $_POST['pass'];
 
+		if(empty($user_pass)||empty($user_name)){
+			$mess = notification('Input fild empty','warning');
+		}else{
+
+				$sql = "SELECT * FROM user_info WHERE uname = '$user_name'|| email = '$user_name'";
+				$data = $connection -> query($sql);
+				$login_user = $data -> fetch_assoc();
+				//pasword verify
+				if($data -> num_rows == 1){
+					if (password_verify($user_pass, $login_user['pass'])) {
+						
+
+						//session data
+						$_SESSION['id'] = $login_user['id'];
+						$_SESSION['name'] = $login_user['name'];
+						$_SESSION['email'] = $login_user['email'];
+						$_SESSION['phone'] = $login_user['phone'];
+						$_SESSION['photo'] = $login_user['photo'];
+						$_SESSION['uname'] = $login_user['uname'];
+
+						//cookie setup
+						setcookie('relog',$login_user['id'],time() + (60*60*24*365));
+						
+						// redirect user to profile page
+						header("location:profile.php");
+
+					}else{
+						$_SESSION['id'] = $login_user['id'];
+						header("location:user_pass_verification.php");
+					}
+				}else{
+					$mess = notification('Email | user not found','danger');
+				}
+		}
 	
 
-		if(empty($name)||empty($email)||empty($phone)||empty($Address)){
-			$mess= '<p class="alert alert-danger alert-dismissible fade show">Data fild is empty <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				    <span aria-hidden="true">&times;</span>
-				  </button></p>';
-		}elseif(filter_var($email, FILTER_VALIDATE_EMAIL)== false) {
-			$mess= '<p class="alert alert-warning alert-dismissible fade show">Evalid email<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				    <span aria-hidden="true">&times;</span>
-				  </button></p>';
-		} else{
-
-
-			$data = fileUp($_FILES['photo'], "photos/");
-			$photo_name_str = $data['file_name'];
-
-			if ($data['status'] == 'yes') {
-
-				$sql = "INSERT INTO user_info (Name,Email,Phone,Adress,photo_str) VALUES ('$name','$email','$phone','$Address','$photo_name_str')" ;
-				$connection -> query($sql); 
-			
-
-				$mess= '<p class="alert alert-success alert-dismissible fade show">Data is stable<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				    <span aria-hidden="true">&times;</span>
-				    </button></p>';
-			}else{
-				$mess= '<p class="alert alert-warning alert-dismissible fade show">Image formet error<button type="button" class="close" data-dismiss="alert" aria-label="Close">
-				    <span aria-hidden="true">&times;</span>
-				    </button></p>';
-			};
-		};
+	
 	};
 
 	 ?>
-	 <?php
-	 if (isset($name)) {
-		
-			
-				echo $mess; 
-			
-		
-	};
-	?>
+	 
+	<div class="row">
+	    <div class="col ">
+	    	<div class="mx-auto">
+	     <!-- recent log in div start -->
+		<?php 
+		// recent login data recive using cookie id start
+		if (isset($_COOKIE['rememLogin'])) {
+			$rlogin_id = $_COOKIE['rememLogin'];
 
-	
-	<div class="card shadow">
-		<a href="all.php">show all user</a>
+			$sql = "SELECT * FROM user_info WHERE id = '$rlogin_id'";
+				$data = $connection -> query($sql);
+				$login_user = $data -> fetch_assoc();
+
+		}
+		//recent login data recive using cookie id End
+		if (isset($_COOKIE['rememLogin'])): ?>
+
+			<div class="car shadow w-25 mx-auto" style="margin-top:7%; margin-left: 2%">
+				<a href="?triger=<?php echo $login_user['id'];?>">
+					<div class="card-body w-25">
+					<img style="max-height: 100px;" src="photos/<?php echo $login_user['photo'];?>" alt="">
+					</div>
+				</a>
+				
+				<div class="card-footer">
+					<a href="?task=clear">Clear</a> <?php echo $login_user['uname'];?>
+				</div>
+			</div>
+		<?php endif ?>
+		
+		<?php 
+		if (isset($_GET['triger'])) {
+			// redirect user to profile page
+			$each_id = $_GET['triger'];
+
+			$sql = "SELECT * FROM user_info WHERE id = '$each_id'";
+				$data = $connection -> query($sql);
+				$each_user = $data -> fetch_assoc();
+				// session create by cooki id
+				$_SESSION['id'] = $each_user['id'];
+				// $_SESSION['name'] = $each_user['name'];
+				// $_SESSION['email'] = $each_user['email'];
+				// $_SESSION['phone'] = $each_user['phone'];
+				// $_SESSION['photo'] = $each_user['photo'];
+				// $_SESSION['uname'] = $each_user['uname'];
+
+				// redirect user to profile page
+				header("location:user_pass_verification.php");
+			}
+		
+
+		 ?>
+		
+
+		<!-- recent log in disable start -->
+		<?php
+		if (isset($_GET['task']) AND ($_GET['task'] = 'clear') ) {
+			setcookie('rememLogin','',time() - (60*60*24*365));
+			header("location:index.php");
+		}
+		
+		 ?>
+		 <!-- recent log in disable End -->
+		<!-- recent log in div End -->
+			</div>
+	    </div>
+	    <div class="col">
+	     
+			<div class="card shadow w-50 mx-auto" style="margin-top: 5%;">
+				<?php
+				if (isset($mess)) {
+					echo $mess; 
+					
+				}
+
+				?>	
+
 	<div class="card-body">
 		<div class="card-head">
-			<h2>Registaion</h2>
+			<h2>Login</h2></br>
 		</div>
 		<form  action="" method="POST" enctype="multipart/form-data">
 		<div class = "form-group">
-			<label>Name:</label>
+			<label>User name:</label>
 			<input type="text" class="form-control" name="name" value="<?php old('name');?>" placeholder="User Name">
 		</div>
 
 		<div class = "form-group">
-			<label>Email:</label>
-			<input type="text" class="form-control" name="email" value="<?php old('email');?>" placeholder="User Email">
+			<label>Password:</label>
+			<input type="password" class="form-control" name="pass" value="" placeholder="Password">
 		</div>
 
-	
-		<div class = "form-group">
-			<label>Phone:</label>
-			<input type="text" class="form-control" name="phone" value="<?php old('phone');?>"  placeholder="User Phone Number">
-		</div>
-
-		<div class = "form-group">
-			<label>Adress:</label>
-			<input type="text" class="form-control" name="add" value="<?php old('add');?>" placeholder="User adress">
-		</div>
-
-		<div class = "form-group">
-			
-			<input type="file" name="photo">
-		</div>
 		<div class = "form-group">
 			
 			<input type="submit" class="form-control bg-success" name="save" value="Save" >
@@ -106,9 +189,17 @@
 
 
 		</form>
+		<div class="card-footer">
+			<a href="reg.php">Create Account</a>
+		</div>
 		
 	</div>
 </div>
+
+	    </div>
+  </div>
+	
+
 	<script src="asset/js/jquery-3.4.1.min.js"></script>
 	<script src="asset/js/popper.min.js"></script>
 	<script src="asset/js/bootstrap.min.js"></script>
